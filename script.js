2fa256e904d3666bb52a10083e9ea62b7f12f68f
Cameron <cameronbrady1527@gmail.com>
2025-04-29 22:46:14 -0400

    properly loading Excel data and storing locally & UI updates

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
-// import 'papaparse';
-// import * as XLSX from 'xlsx/xlsx.js';
-
-let vocabData = [];
+// let vocabData = [];
 
 const handleFileUpload = () => {
   const inputFile = document.getElementById('csv-file');
@@ -18,8 +15,8 @@ const handleFileUpload = () => {
     const fileName = file.name.toLowerCase();
 
     if (fileName.endsWith('.csv')) {
-      parseCSV(data);
-    } else if (fileName.endsWith('.xlsx') || fileName.endsWith('.xls')){
+      // parseCSV(data); // not yet implemented
+    } else if (fileName.endsWith('.xlsx')) {
       parseExcel(data);
     } else {
       alert('Unsupported file format. Please upload a CSV or Excel file.')
@@ -33,47 +30,56 @@ const handleFileUpload = () => {
   }
 }
 
-const parseCSV = (data) => {
-  Papa.parse(data, {
-    header: true,
-    skipEmptyLines: true,
-    complete: function(results) {
-      processData(results.data)
+async function parseExcel(data) {
+  const schema = {
+    'Word': {
+      prop: 'word',
+      type: String,
+      required: true
     },
-    error: function(error) {
-      alert('Error parsing CSV: ' + error);
+    'Type': {
+      prop: 'partOfSpeech',
+      type: String,
+      required: true
+    },
+    'Definition': {
+      prop: 'definition',
+      type: String,
+      required: true
+    },
+    'In a sentence (1)': {
+      prop: 'sentence1',
+      type: String,
+      required: true
+    },
+    'In a sentence (2)': {
+      prop: 'sentence2',
+      type: String,
+      required: true
+    },
+    'Synonyms': {
+      prop: 'synonyms',
+      type: String,
+      required: true
+    },
+    'Times Correct': {
+      prop: 'timesCorrect',
+      type: Number,
+      required: true
+    },
+    'Times Encountered': {
+      prop: 'timesEncountered',
+      type: Number,
+      required: true
     }
-  });
-}
-
-const parseExcel = (data) => {
-  try {
-    const workbook = XLSX.read(data, { type: 'array' });
-    const firstSheetName = workbook.SheetNames[0];
-    const worksheet = worksheet.Sheets[firstSheetName];
-    const jsonData = XLSX.utils.sheet_to_json(worksheet);
-    processData(jsonData);
-  } catch(error) {
-    alert('Error parsing Excel file: ' + error);
   }
-}
 
-const processData = (data) => {
-  vocabData = data.map(item => {
-    const word = {
-      word: item.word || '',
-      partOfSpeech: item['part of speech'] || '',
-      definition: item['definition'] || '',
-      sentence1: item['in a sentence (1)'] || '',
-      semtence2: item['in a sentence (2)'] || '',
-      synonyms: item.synonyms || '',
-      timesCorrect: parseInt(item.times_correct || item.timesCorrect || 0),
-      timesEncountered: parseInt(item.times_encountered || item.timesEncountered || 0),
-    }
-    return word;
-  });
-
-  localStorage.setItem('vocabData', JSON.stringify(vocabData));
+  try {
+    const result = await readXlsxFile(data, { schema });
+    localStorage.setItem('vocabData', JSON.stringify(result.rows));
+  } catch (error) {
+    console.error('Error processing Excel file: ', error);
+  }
 
   // Update UI
   showQuizInterface();
@@ -83,3 +89,6 @@ const showQuizInterface = () => {
   document.getElementById('landing-instructions').style.display = 'none';
   document.getElementById('quiz-container').style.display = 'block';
 }
+
+const showInfo = () => document.getElementById('guessed').style.display = 'block';
+const hideInfo = () => document.getElementById('guessed').style.display = 'none';
